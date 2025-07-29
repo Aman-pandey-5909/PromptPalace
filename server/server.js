508e4e01c149d6004f9663f597c158421997e86a
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-07-29 11:33:42 +0530

    Entry point of the server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
+const cors = require('cors')
+const cp = require('cookie-parser')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
+const tokenValidate = require('./middlewares/tokenValidation')
+const app = express()
+ 
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        credentials: true
+    }
+))
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cp())
+
+const authRoutes = (req, res, next) => {
+    if(req.path.startsWith('/auth')) {
+        return next()
+    }
+    tokenValidate(req, res, next) 
+} 
+
+app.use('/api', authRoutes, require('./routes/index'))
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
