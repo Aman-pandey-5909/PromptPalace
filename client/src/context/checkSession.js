4446644c19cc7c9dc9ef251e6547a590800b8e8c
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-07-30 23:39:48 +0530

    [v0.1 promptpalace: client/context -> for route restriction - prevents DRY] Global Context for Session checking and usage of route restriction from utils dir for route restriction based on api response

---
+++ client/src/context/checkSession.js
@@ -0,0 +1,41 @@
+'use client'
+import { checkRouteRestriction } from "@/utils/checkRouteRestriction";
+import { createContext, useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton"
+
+export const CheckSessionContext = createContext();
+
+const CheckSession = ({ children }) => {
+    const [loading, setLoading] = useState(true)
+    const router = useRouter()
+    const pathname = usePathname()
+    const excludedRoutes = ['/auth/login', '/auth/signup']
+    useEffect(() => {
+        if (excludedRoutes.includes(pathname)) {
+            setLoading(false)
+            return
+        }
+        async function checkRoute() {
+            console.log("session checked");
+            await checkRouteRestriction().then((res) => res.redirect ? router.push(res.redirect.destination) : null)
+            return 
+        }
+        checkRoute()
+        setLoading(false)
+    }, [])
+
+    return (
+        <CheckSessionContext.Provider value={{ loading }}>
+            {loading ? <div className="flex flex-col space-y-3">
+                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <div className="space-y-2">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-4 w-[200px]" />
+                </div>
+            </div> : children }
+        </CheckSessionContext.Provider>
+    )
+}
+export default CheckSession
+
