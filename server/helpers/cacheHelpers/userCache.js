b99f5c99ad6deff81f4f5384a522ba19fe573512
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-07-30 23:17:35 +0530

    [v0.1 promptpalace: cache-related] Shifted user data Cache related functions to helpers directory

---
+++ server/helpers/cacheHelpers/userCache.js
@@ -0,0 +1,37 @@
+const { userDataCache } = require('../../utils/tempCache')
+
+const getUser = (token) => {
+    const user = userDataCache.get(token)
+    return {
+        status: "success",
+        data: user
+    }
+}
+
+const delUser = (token) => {
+    userDataCache.delete(token)
+    return {
+        status: "success",
+        message: "User Cache deleted successfully"
+    }
+}
+
+const setUser = (token, data) => {
+    userDataCache.set(token, data)
+    return {
+        status: "success",
+        message: "User Cache set successfully",
+        data: data
+    }
+}
+
+const getAllUser = () => {
+    return [...userDataCache.entries()]
+}
+
+module.exports = {
+    getUser,
+    delUser,
+    setUser,
+    getAllUser
+}
