5316c8854818fe5bdd5fab25609aeb90b2e3a4f4
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-07-30 23:37:39 +0530

    [v0.1 promptpalace: client/utils -> route restriction function] Check current page , and redirect based on api response -> for route restriction

---
+++ client/src/utils/checkRouteRestriction.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+export async function checkRouteRestriction() {
+    try {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_LINK_VERIFYSESH}`, {}, { withCredentials: true });
+
+    } catch (error) {
+        return {
+            message: 'You are not logged in',
+            redirect: {
+                destination: '/auth/login',
+                permanent: false
+            }
+        }
+    }
+}
