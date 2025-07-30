fc14d485928efe64af950148714ddd1901f1e9c8
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-07-30 23:27:53 +0530

    [v0.1 promptpalace: controller related] Changing to DRY by using cache methods from /helpers/cacheHelpers

--- server/controllers/auth/login.controller.js
+++ server/controllers/auth/login.controller.js
@@ -2,7 +2,8 @@ const bcryptjs = require('bcryptjs')
 const fs = require('fs')
 const path = require('path')
 const jwt = require('jsonwebtoken')
-const { loginCache, userDataCache } = require('../../utils/tempCache')
+const { setUser, getAllUser } = require('../../helpers/cacheHelpers/userCache')
+const { setLogin, getAllLogin } = require('../../helpers/cacheHelpers/loginCache')
 const REGEXFORAUTH = require('../../utils/regexes')
 const getDataFromRequest = require('../../utils/getDataFromRequest')
 /**
@@ -44,15 +45,15 @@ exports.login = async (req, res) => {
         }
 
         const token = jwt.sign({ id: existingUser.id, email: existingUser.email, username: existingUser.username }, process.env.JWT_SECRET, { expiresIn: '1d' }) // create a token
-        loginCache.set(token, Date.now() + 24 * 60 * 60 * 1000) // cache the token with its value as expiration time
-        userDataCache.set(token, { id: existingUser.id, email: existingUser.email, username: existingUser.username })
+        setLogin(token, Date.now() + 24 * 60 * 60 * 1000) // cache the token with its value as expiration time
+        setUser(token, { id: existingUser.id, email: existingUser.email, username: existingUser.username })
         fs.writeFileSync(path.join(__dirname, '../../tempJSONDATA/User.json'), JSON.stringify([...users.map(user => user.id === existingUser.id ? { ...user, token } : user)]), 'utf-8') // update the user with the new token, adds token key and assigns the token value
 
         res.cookie('userData', token, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 24 * 60 * 60 * 1000 }) // sets a userData cookie on the response whose maxAge is 24hrs, used as main cookie
         res.cookie('tempUserData', token, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 10 * 60 * 1000 }) // sets a tempUserData cookie on the response whose maxAge is 10min, used as backup cookie
 
-        console.log("LoginTokens: ", [...loginCache.entries()]);
-        console.log("UserTokens: ", [...userDataCache.entries()]);
+        console.log("LoginTokens: ", getAllLogin());
+        console.log("UserTokens: ", getAllUser());
 
         return res.status(200).json({ message: 'User logged in successfully' })
 
--- server/controllers/auth/register.controller.js
+++ server/controllers/auth/register.controller.js
@@ -7,31 +7,31 @@ const getDataFromRequest = require('../../utils/getDataFromRequest')
 exports.register = async (req, res) => {
     try {
 
-        const data = getDataFromRequest(req, {withId: true, withTimestamp: true})
+        const data = getDataFromRequest(req, { withId: true, withTimestamp: true })
         const { username, email, password, confirmPassword, id } = data
 
         if (!username || !email || !password || !confirmPassword) {
-            return res.status(400).json({message: 'All fields are required'})
+            return res.status(400).json({ message: 'All fields are required' })
         }
 
         if (!REGEXFORAUTH.email.test(email)) {
-            return res.status(400).json({message: 'Invalid email'})
+            return res.status(400).json({ message: 'Invalid email' })
         }
 
         if (!REGEXFORAUTH.password.test(password)) {
-            return res.status(400).json({message: 'Invalid password'})
+            return res.status(400).json({ message: 'Invalid password' })
         }
 
         if (password !== confirmPassword) {
-            return res.status(400).json({message: 'Passwords do not match'})
+            return res.status(400).json({ message: 'Passwords do not match' })
         }
 
         const user = fs.readFileSync(path.join(__dirname, '../../tempJSONDATA/User.json'), 'utf-8')
         const users = JSON.parse(user)
-        
+
         const existingUser = users.find(user => user.email === email)
         if (existingUser) {
-            return res.status(400).json({message: 'User already exists'})
+            return res.status(400).json({ message: 'User already exists' })
         }
 
         const hashedPassword = await bcryptjs.hash(password, 10)
@@ -44,10 +44,10 @@ exports.register = async (req, res) => {
         }
 
 
-        const saveUser = fs.writeFileSync(path.join(__dirname, '../../tempJSONDATA/User.json'), JSON.stringify([...users, {...newUserData} ]), 'utf-8')
+        const saveUser = fs.writeFileSync(path.join(__dirname, '../../tempJSONDATA/User.json'), JSON.stringify([...users, { ...newUserData }]), 'utf-8')
 
 
-        return res.status(200).json({message: 'User registered successfully', data: saveUser})
+        return res.status(200).json({ message: 'User registered successfully', data: saveUser })
 
     } catch (error) {
         console.log(error)
