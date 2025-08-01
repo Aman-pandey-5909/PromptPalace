19a2d177986c0a818e5361e6939c17fd5650d62f
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-01 10:46:34 +0530

    [v0.1 promptpalace : utils/regexes] added regex for username, mobileno, country

--- server/utils/regexes.js
+++ server/utils/regexes.js
@@ -1,7 +1,9 @@
 const REGEXFORAUTH = {
     email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
     password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-    username: /^[a-zA-Z0-9]{3,}$/
+    username: /^[a-zA-Z0-9]{3,}$/ ,
+    mobileNumber: /^\+?\d{10,15}$/ ,
+    country: /^[a-zA-Z]{2,}$/
 }
 
 module.exports = REGEXFORAUTH
