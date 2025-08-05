02cf496a5253ab4426e91747b0d8f9a5e4d55b31
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-06 00:11:20 +0530

    [PromptPalace v0.2 | client related - /dashboard ] dashboard page

---
+++ client/src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+'use client'
+import Dashboard from "@/components/dashboard/Dashboard"
+import React from "react"
+
+const page = () => {
+  return (
+    <React.Fragment>
+      <Dashboard />
+    </React.Fragment>
+  )
+}
+export default page
