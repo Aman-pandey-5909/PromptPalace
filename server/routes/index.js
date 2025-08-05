87c45c281a23f84420412aca1b33502d569fcb95
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-05 23:59:22 +0530

    [PromptPalace v0.1 | Server related - route/index.js] common point of all the routes => added feed and user route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-//AUTH ROUTES
+//ROUTE IMPORTS
 const loginRoute = require('./auth/login.route')
 const registerRoute = require('./auth/register.route')
 const logoutRoute = require('./auth/logout.route')
 const verifySeshRoute = require('./auth/verifysesh.route')
-
+const postsRoute = require('./feed/posts.route')
+const detailsRoute = require('./user/details.route')
 
 //AUTH ROUTES
 router.use('/auth', loginRoute)
@@ -15,7 +16,8 @@ router.use('/auth', logoutRoute)
 
 // Verify Session route for route restriction in client
 router.use(verifySeshRoute)
-
+router.use(postsRoute)
+router.use(detailsRoute)
 
 
 module.exports = router
