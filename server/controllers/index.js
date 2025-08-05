3d75760c00a11b767a670a3c900fc8297f06f407
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-06 00:04:09 +0530

    [PromptPalace v0.1 | Server related - controllers/index ] common point of all controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,5 +2,7 @@ module.exports = {
     ...require('./auth/login.controller'),
     ...require('./auth/register.controller'),
     ...require('./auth/logout.controller'),
-    ...require('./auth/verifysesh.controller')
+    ...require('./auth/verifysesh.controller'),
+    ...require('./feed/posts.controller'),
+    ...require('./user/details.controller')
 }
