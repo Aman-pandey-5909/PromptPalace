f976db8339418eb6f4ec0085bf5cb9d821108777
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-06 00:10:46 +0530

    [PromptPalace v0.2 | client related - layout.js ] Implemented getUser context

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CheckSession from "@/context/checkSession";
+import GetUser from "@/context/getUser";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CheckSession>
-          {children}
+          <GetUser>{children}</GetUser>
         </CheckSession>
       </body>
     </html>
