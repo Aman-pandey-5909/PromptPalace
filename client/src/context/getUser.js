60e707400784e4b5b411a6c59a81bee1a8cf1799
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-06 00:07:54 +0530

    [PromptPalace v0.2 | client related - get User details in global state ]

---
+++ client/src/context/getUser.js
@@ -0,0 +1,31 @@
+'use client'
+
+import { createContext, useEffect, useState } from "react"
+import axios from "axios";
+import { usePathname } from "next/navigation";
+
+export const GetUserContext = createContext();
+
+export default function GetUser ({children}) {
+    const [userData, setUserData] = useState({});
+    const pathname = usePathname()
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_LINK}/details`, { withCredentials: true })
+                setUserData(res.data.data)
+                console.log(res.data.data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getUser()
+    }, [ pathname ])
+
+    return (
+        <GetUserContext.Provider value={{userData, setUserData}}>
+            {children}
+        </GetUserContext.Provider>
+    )
+
+}
