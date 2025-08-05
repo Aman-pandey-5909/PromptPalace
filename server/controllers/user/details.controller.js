1de256c45a4672a48c8a78833b9353a9f7818ce9
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-06 00:03:40 +0530

    [PromptPalace v0.1 | Server related - controllers/user ] GET api for client side to get userData

---
+++ server/controllers/user/details.controller.js
@@ -0,0 +1,14 @@
+const {getUser} = require('../../helpers/cacheHelpers/userCache')
+
+exports.details = async (req, res) => {
+    try {
+        const usercookietoken = req.cookies.userData
+        console.log(usercookietoken);
+        const user = getUser(usercookietoken)
+        // console.log(user);
+        return res.status(200).json({message: 'User details fetched', data: user.data})
+    } catch (error) {
+        console.error(error);  
+        return res.status(500).json({message: 'Internal server error'})
+    }
+} 
