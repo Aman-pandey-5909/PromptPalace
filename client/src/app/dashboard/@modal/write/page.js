dd865fcdd8ec741351717b089382d9a7e1b662ab
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-11 15:16:15 +0530

    [PromptPalace v0.2 | client | /dashboard/writepost ] onSubmit() function added

--- client/src/app/dashboard/@modal/write/page.js
+++ client/src/app/dashboard/@modal/write/page.js
@@ -1,12 +1,22 @@
 "use client"
 import WritePost from "@/components/dashboard/WritePost"
 import { useRouter } from "next/navigation"
+import axios from "axios"
 
 const page = () => {
   const router = useRouter()
+  const onSubmit = async (data) => {
+    try {
+      const res = await axios.post(`${process.env.NEXT_PUBLIC_API_LINK}/postprompt`, data, { withCredentials: true })
+      console.log(res)
+      router.back()
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black/40 backdrop-blur-sm">
-      <WritePost />
+      <WritePost onSubmit={onSubmit} />
       <button onClick={() => router.back()}>Back</button>
     </div>
   )
