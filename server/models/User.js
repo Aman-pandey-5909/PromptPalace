6f14763bb9605058dc989db717f848728b488653
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-11 15:22:02 +0530

    [PromptPalace v0.2 | server | models/User  ] shifted to default ObjectId from custom id using uuid

--- server/models/User.js
+++ server/models/User.js
@@ -12,10 +12,6 @@ const mongoose = require("mongoose");
  */
 
 const userSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        unique: true  
-    },
     name: {
         type: String,
         trim: true,
