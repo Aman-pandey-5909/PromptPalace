a83b6187add8ebf70e0e2cc9bd44f107fd05d0d0
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-11 15:28:16 +0530

    [PromptPalace v0.2 | server | controllers/index ] new controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,5 +4,7 @@ module.exports = {
     ...require('./auth/logout.controller'),
     ...require('./auth/verifysesh.controller'),
     ...require('./feed/posts.controller'),
-    ...require('./user/details.controller')
+    ...require('./user/details.controller'),
+    ...require('./user/updatedetails.controller'),
+    ...require('./posts/postprompt.controller')
 }
