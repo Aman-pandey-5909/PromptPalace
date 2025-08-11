b2823f6ccb1309603f631a66e0e24fb2fac2e3f1
Aman-pandey-5229 <amandbz5909@gmail.com>
2025-08-11 15:18:27 +0530

    [PromptPalace v0.2 | server | routes  ] new routes added

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,8 @@ const logoutRoute = require('./auth/logout.route')
 const verifySeshRoute = require('./auth/verifysesh.route')
 const postsRoute = require('./feed/posts.route')
 const detailsRoute = require('./user/details.route')
+const updateDetailRoute = require('./user/updatedetails.route')
+const postPromptRoute = require('./posts/postprompt.route')
 
 //AUTH ROUTES
 router.use('/auth', loginRoute)
@@ -16,8 +18,12 @@ router.use('/auth', logoutRoute)
 
 // Verify Session route for route restriction in client
 router.use(verifySeshRoute)
+
+// Other Routes
 router.use(postsRoute)
 router.use(detailsRoute)
+router.use(updateDetailRoute)
+router.use(postPromptRoute)
 
 
 module.exports = router
